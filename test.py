35eb5787b37699921d61d74d3f341b4af7361a7f
Drew Devero <deverobelfon@gmail.com>
2021-02-27 22:15:23 -0500

    added async function where await applies within a loop

--- test.py
+++ test.py
@@ -15,9 +15,13 @@ import asyncio
 """ pow(), sqrt(), mix(), max() and some other functions do not require from math import * """
 
 my_name = "Alston"
-my_power_level = "Also, my power level is over " + str(pow(10, 3) * 9) + "!!!"
+my_power_level = "Also, my power level is over " + str(pow(10, 3) * 9) + "!!!" + "Also, also, my favorite number is: " + str(round(2.7))
 initial_greeting = "Hey, it's " + my_name + ". Training and testing for Tri-Sense will live here, eventually."
 names_and_numbers = ["Carl", str(3), "Stan", str(7), "Ken"]
+
+""" Tupple time with () like - tupple_time = ("index 0 here", "index 1 here") """
+""" Tupples are immutable """
+""" Can access tupples with indexing like - tupple_time[1]"""
 coordinate = (5, 3)
 coordinates = [(2, 3), (8, 10), (20, 15)]
 
@@ -31,4 +35,19 @@ async def show_nums_and_coordinates(confirmation):
     await asyncio.sleep(3)
     print("Thank you for trying this program.")
 
-asyncio.run(show_nums_and_coordinates("All Good Here"))
+async def working_with_list_flow():
+    favorite_numbers = [3, 5, 7, 15]
+    favorite_characters = ["Goku", "Midoriya", "Andy Dufresne", "Edmond Dantes"]
+    favorite_characters.extend(favorite_numbers)
+    fav_nums_copy = favorite_numbers.copy()
+    favorite_numbers.extend(favorite_characters)
+    print(fav_nums_copy)
+    print(favorite_characters)
+    for number in favorite_numbers:
+        await asyncio.sleep(1)
+        print(number)
+    else:
+        print("Loop complete!")
+
+asyncio.run(working_with_list_flow())
+asyncio.run(show_nums_and_coordinates("all Good Here"))
